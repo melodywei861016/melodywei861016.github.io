44cae44c9cc3e8ac67ef7bf6e0da9f8411603c0c
Melody Wei <melodywei861016@gmail.com>
2018-09-08 18:33:14 -0700

    initial design

--- personal-website/src/components/headerComponent/header.js
+++ personal-website/src/components/headerComponent/header.js
@@ -4,20 +4,22 @@ class Header extends Component {
   render() {
     return (
       <header>
-        <div className="logo">
-          LOGO
-        </div>
-
         <nav>
           <ul>
-            <li>
-              <a href="#">Home</a>
+            <li className="first">
+              <a href="/">HOME</a>
             </li>
             <li>
-              <a href="#">Products</a>
+              <a href="Projects">PROJECTS</a>
             </li>
             <li>
-              <a href="#">Contact</a>
+              <a href="https://drive.google.com/file/d/11RnzZRcJOyB1kInKImCXO49Vwpa-s7ZC/view?usp=sharing">RESUME</a>
+            </li>
+            <li>
+              <a href="https://www.linkedin.com/in/wei-melody/">LINKEDIN</a>
+            </li>
+            <li className="last">
+              <a href="https://github.com/melodywei861016">GITHUB</a>
             </li>
           </ul>
         </nav>
--- personal-website/src/components/pages/homePage.js
+++ personal-website/src/components/pages/homePage.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
+import ProPic from '../../Assets/images/pro_pic_circle.png';
 
 class Homepage extends Component {
   render() {
     return (
-      <div className="container-fluid">
-        Home Page Content Here
+      <div className="homepage">
+        <h1>
+          Melody Wei
+        </h1>
+        <p>
+          <img src={ProPic} width="500" height="500"/>
+        </p>
       </div>
     );
+
   }
 }
 
