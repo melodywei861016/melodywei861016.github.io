39d6826505d221bed838361ffca4c39016bf432e
Melody Wei <melodywei861016@gmail.com>
2018-09-29 15:03:24 -0700

    personal website

--- personal-website/src/components/pages/homePage.js
+++ personal-website/src/components/pages/homePage.js
@@ -16,20 +16,14 @@ class Homepage extends Component {
           <p2>
             <b>Hello!</b> I'm a junior studying Electrical Engineering and Computer Science at UC Berkeley.<br/>
             Currently looking for a software engineering internship for Summer 2019 and am <br/> interested in frontend & mobile/web development.
-            Interned as Engineering Practicum <br/>Intern at <u1><font color="#FF3300"><b>Google</b></font></u1> in Summer 2017, and as Android Developer Intern at <u2><font color="#9933FF"><b>Mammoth Media</b></font> <br/></u2>in summer 2018.
-            I also developed mobile/web apps for <u3><font color="#3366FF"><b>Blueprint, Technology for Nonprofit</b></font></u3>.
+            Interned as Engineering Practicum <br/>Intern at <a href="/Google"><u1><b>Google</b></u1></a> in Summer 2017, and as Android Developer Intern at <a href="/Mammoth"><u2><b>Mammoth Media</b><br/></u2></a>in summer 2018.
+            I also developed mobile/web apps for <a href="/Blueprint"><u3><b>Blueprint, Technology for Nonprofit</b></u3></a>.
           </p2>
         </p>
         <p3>
-          <p4>
-            <img src={Mammoth} width="300" height="170"/>
-          </p4>
-          <p5>
-            <img src={Google} width="300" height="170"/>
-          </p5>
-          <p6>
-            <img src={Blueprint} width="300" height="170"/>
-          </p6>
+            <img src={Mammoth} width="300" height="170" class="mammoth"/>
+            <img src={Google} width="300" height="170" class="google"/>
+            <img src={Blueprint} width="300" height="170" class="blueprint"/>
         </p3>
       </div>
     );
