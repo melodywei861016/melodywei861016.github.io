d3204190bacc3560f4f34fa4ddab09d44473e20e
Melody Wei <melodywei861016@gmail.com>
2022-07-01 00:55:54 -0700

    Updates

---
+++ precache-manifest.9a5dfd4f26182d121930983a7c8602be.js
@@ -0,0 +1,194 @@
+self.__precacheManifest = [
+  {
+    "revision": "4dc67fc2da3aa2c28054",
+    "url": "/static/css/main.853886ea.chunk.css"
+  },
+  {
+    "revision": "4dc67fc2da3aa2c28054",
+    "url": "/static/js/main.cd0953e8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "fb103dc726bf771bc79f",
+    "url": "/static/js/2.3c592117.chunk.js"
+  },
+  {
+    "revision": "5b1ab40c3ebf83995164a971d7aa8377",
+    "url": "/static/media/melody_photo.5b1ab40c.JPG"
+  },
+  {
+    "revision": "344fea7e70c37d2e5dd55332327efa46",
+    "url": "/static/media/instagram.344fea7e.png"
+  },
+  {
+    "revision": "898c0407d74c75fa110de76293d96ddd",
+    "url": "/static/media/linkedin.898c0407.png"
+  },
+  {
+    "revision": "c28071fcea9742f039d0ae44468caf22",
+    "url": "/static/media/tiktok.c28071fc.png"
+  },
+  {
+    "revision": "d531be7f753e52ba4f000b09f7c9b01c",
+    "url": "/static/media/email.d531be7f.png"
+  },
+  {
+    "revision": "8bbf193f4622bd0e9b1ad68204ef1838",
+    "url": "/static/media/youtube.8bbf193f.png"
+  },
+  {
+    "revision": "474007496611cd00703070d166961a51",
+    "url": "/static/media/rebrand.47400749.png"
+  },
+  {
+    "revision": "109329a88c5e7d0932d21e1ab8909ff3",
+    "url": "/static/media/split_capture_main_photo.109329a8.png"
+  },
+  {
+    "revision": "6c247f81e38013782d74d9e5e74346c2",
+    "url": "/static/media/account_creation_main_photo.6c247f81.png"
+  },
+  {
+    "revision": "8fd2452a09802abdc17d4cb85b45c999",
+    "url": "/static/media/onboarding_funnel_main_photo.8fd2452a.png"
+  },
+  {
+    "revision": "cdf20358f7d2279988f235f2f3694e85",
+    "url": "/static/media/mobile_responsive_main_photo.cdf20358.png"
+  },
+  {
+    "revision": "00d07ec4c9bb9354f1831a05ddcdd00a",
+    "url": "/static/media/personal_website_main_photo.00d07ec4.png"
+  },
+  {
+    "revision": "aeffa0dd622053026d97d164961acd2a",
+    "url": "/static/media/rebrand_logo_change.aeffa0dd.png"
+  },
+  {
+    "revision": "eae1d8c32b230d5f4b2b4b6fa3532068",
+    "url": "/static/media/rebrand_color_change.eae1d8c3.png"
+  },
+  {
+    "revision": "0381c8290a928b5619ca9e7f4ab83f30",
+    "url": "/static/media/rebrand_typography_change.0381c829.png"
+  },
+  {
+    "revision": "8b34d6834e9fc800891b74cf5a4cacc5",
+    "url": "/static/media/promo_old.8b34d683.png"
+  },
+  {
+    "revision": "495be011a16931fc465b997035ba243e",
+    "url": "/static/media/promo_new.495be011.png"
+  },
+  {
+    "revision": "e887b40ce43f76c98983f75dee516e0c",
+    "url": "/static/media/fp_old.e887b40c.png"
+  },
+  {
+    "revision": "5ee65f220d49ac52bb7f6bd602eef165",
+    "url": "/static/media/fp_new.5ee65f22.png"
+  },
+  {
+    "revision": "bd72f000f1e5d9ecbd178fc7e5e2bd49",
+    "url": "/static/media/home_old.bd72f000.png"
+  },
+  {
+    "revision": "e18df0c9618433f1c8c3087f6b09aada",
+    "url": "/static/media/home_new.e18df0c9.png"
+  },
+  {
+    "revision": "735678a61508592cdd5078814a9d6fbb",
+    "url": "/static/media/login_old.735678a6.png"
+  },
+  {
+    "revision": "ce6554077d9bc541b244985efd39eb05",
+    "url": "/static/media/login_new.ce655407.png"
+  },
+  {
+    "revision": "714341675c3bc62ada1c76ac83c52ecd",
+    "url": "/static/media/split_capture_recording.71434167.mov"
+  },
+  {
+    "revision": "c0017783f420415fb9c5252599d7478c",
+    "url": "/static/media/melody_resume.c0017783.jpg"
+  },
+  {
+    "revision": "bd50549cab1bba3ed1420defc02af56a",
+    "url": "/static/media/account_creation_recording.bd50549c.mov"
+  },
+  {
+    "revision": "8f73a6cecc16dda1c2497b337642d65f",
+    "url": "/static/media/activation_video.8f73a6ce.png"
+  },
+  {
+    "revision": "5706b538226747c924f555226d218c78",
+    "url": "/static/media/onboarding_funnel_approved.5706b538.png"
+  },
+  {
+    "revision": "afec5e973357321f95631e5e8aaf4fbd",
+    "url": "/static/media/onboarding_funnel_pending.afec5e97.png"
+  },
+  {
+    "revision": "86cadd494cd73d71ab2c2d0aefd5cc5c",
+    "url": "/static/media/onboarding_funnel_blur_api.86cadd49.png"
+  },
+  {
+    "revision": "7d488717f9c286d54a516a4b76b58120",
+    "url": "/static/media/onboarding_fp_recording.7d488717.mov"
+  },
+  {
+    "revision": "a6728a4b0977e71dbb426601ce246221",
+    "url": "/static/media/promo_status_screen_recording.a6728a4b.mov"
+  },
+  {
+    "revision": "b91126aa88987508fffe8b10956bc58c",
+    "url": "/static/media/onboarding_package_selection_recording.b91126aa.mov"
+  },
+  {
+    "revision": "a0b513a9b0425726eebbbf51d46dd274",
+    "url": "/static/media/onboarding_package_confirmation_recording.a0b513a9.mov"
+  },
+  {
+    "revision": "a17f43cc60643d965636985afc00a221",
+    "url": "/static/media/Montserrat-Light.a17f43cc.ttf"
+  },
+  {
+    "revision": "24ba6c180e414dc24bbf34fe4c895f41",
+    "url": "/static/media/Varela-Regular.24ba6c18.ttf"
+  },
+  {
+    "revision": "aca6287f22eef510c1e622c97bb1e1e1",
+    "url": "/static/media/Montserrat-Medium.aca6287f.ttf"
+  },
+  {
+    "revision": "34de1239b12123b85ff1a68b58835a1f",
+    "url": "/static/media/Montserrat-Regular.34de1239.ttf"
+  },
+  {
+    "revision": "a9bf8ef62ed0df09717e5f672116fb16",
+    "url": "/static/media/DidactGothic-Regular.a9bf8ef6.ttf"
+  },
+  {
+    "revision": "dc2f156b60f53c591fc0d2b98cbf01bf",
+    "url": "/static/media/Montserrat-ExtraBold.dc2f156b.ttf"
+  },
+  {
+    "revision": "7ffeec2b4edb434d393875ffbe633c30",
+    "url": "/static/media/Montserrat-SemiBold.7ffeec2b.ttf"
+  },
+  {
+    "revision": "d14ad1035ae6da4e5a71eca362a8d696",
+    "url": "/static/media/Montserrat-Bold.d14ad103.ttf"
+  },
+  {
+    "revision": "55e37a35525c63e130e76d588f2f5e8d",
+    "url": "/static/media/Montserrat-Black.55e37a35.ttf"
+  },
+  {
+    "revision": "98dd0711c471630a26ec9e4606f3df60",
+    "url": "/index.html"
+  }
+];
